d60914e74ba3ce3d6a0cad05f94458a5c6537afc
u9lyfacek1d <uglyyyfaceeekiddd@gmail.com>
2025-08-11 16:54:38 +0200

    Update internal.py

--- app/handlers/internal.py
+++ app/handlers/internal.py
@@ -1,7 +1,64 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, UploadFile
 from sqlalchemy.orm import Session
 from app import models, schemas, crud
 from app.handlers.external import fetch_books_from_google
+import pandas as pd
+import os
+import tempfile
+from fastapi.responses import FileResponse
+
+
+async def export_books_handler(db: Session, file_format: str = "xlsx"):
+    books = db.query(models.Book).all()
+    if not books:
+        raise ValueError("Нет данных для экспорта")
+
+    data = [
+        {
+            "ID": b.id,
+            "Title": b.title,
+            "Author": b.author,
+            "Year": b.year,
+        }
+        for b in books
+    ]
+
+    df = pd.DataFrame(data)
+
+    tmp_dir = tempfile.gettempdir()
+    file_path = os.path.join(tmp_dir, f"books_export.{file_format}")
+
+    #
+    df.to_excel(file_path, index=False, engine="openpyxl")
+
+    return FileResponse(
+        file_path,
+        filename=f"books_export.{file_format}",
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+async def import_books_from_excel(file: UploadFile, db: Session):
+    contents = await file.read()
+    df = pd.read_excel(contents)
+
+    required_columns = {"title", "author", "year"}
+    if not required_columns.issubset(df.columns):
+        missing = required_columns - set(df.columns)
+        raise ValueError(f"Отсутствуют колонки: {', '.join(missing)}")
+
+    imported_count = 0
+
+    for _, row in df.iterrows():
+        book = models.Book(
+            title=row["title"],
+            author=row["author"],
+            year=int(row["year"]) if not pd.isna(row["year"]) else None
+        )
+        db.add(book)
+        imported_count += 1
+
+    db.commit()
+    return imported_count
 
 async def get_books_by_filters(db: Session, filters, skip: int, limit: int):
     query = db.query(models.Book)
